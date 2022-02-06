7b488729916ab37ee89f519bf58143db7942d96b
arkavletu <96702972+arkavletu@users.noreply.github.com>
2022-02-07 00:31:44 +0300

    Update main.kt
    changed fun spell

--- src/main/kotlin/ru/netology/main.kt
+++ src/main/kotlin/ru/netology/main.kt
@@ -3,10 +3,10 @@ package ru.netology
 fun main() {
     val likes = 235
 
-    println(spelling(likes))
+    println(spell(likes))
 
 }
-fun spelling(likes: Int): String{
+fun spell(likes: Int): String{
     val likesToString = likes.toString()
     val spelling = if (likesToString.last() == '1') "человеку" else "людям"
     return "Понравилось $likes $spelling"
