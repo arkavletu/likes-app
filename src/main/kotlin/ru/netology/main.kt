8cec65ecad5fd7fd9c04770316b25e4dcd7404d2
Alina Prokofeva <whitestring@inbox.ru>
2022-02-09 21:49:12 +0300

    changed fun spell

--- src/main/kotlin/ru/netology/main.kt
+++ src/main/kotlin/ru/netology/main.kt
@@ -1,15 +1,12 @@
 package ru.netology
 
 fun main() {
-    val likes = 231
+    val likes = 1
     val result = spell(likes)
     println(result)
 
 }
 fun spell(likes: Int): String{
-    val spelling = when {
-        (likes == 1) || (likes % 10 == 1) && (likes !=11) && (likes % 100 != 11) -> "человеку"
-        else -> "людям"
-    }
+    val spelling = if ((likes % 10 == 1) && (likes !=11) && (likes % 100 != 11)) "человеку" else "людям"
     return "Понравилось $likes $spelling"
 }
