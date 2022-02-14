188d03463d104f09a6e9ad6ecf3230a368b34720
Alina Prokofeva <whitestring@inbox.ru>
2022-02-14 13:21:21 +0300

    checked maths

--- src/main/kotlin/ru/netology/main.kt
+++ src/main/kotlin/ru/netology/main.kt
@@ -1,12 +1,12 @@
 package ru.netology
 
 fun main() {
-    val likes = 1
+    val likes = 111
     val result = spell(likes)
     println(result)
 
 }
 fun spell(likes: Int): String{
-    val spelling = if ((likes % 10 == 1) && (likes !=11) && (likes % 100 != 11)) "человеку" else "людям"
+    val spelling = if (likes % 100 != 11) "человеку" else "людям"
     return "Понравилось $likes $spelling"
 }
